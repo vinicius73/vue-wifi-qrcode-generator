b79ae0c49955888df635c95647679b24988c0c8b
Vinicius Reis <luiz.vinicius73@gmail.com>
2020-08-16 01:35:09 -0300

    🎉 init

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container mx-auto">
+    <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+      <strong class="font-bold">Hello!</strong>
+      <span class="block sm:inline">Init Project.</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  purge: {
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      './index.html',
+      './src/**/*.vue',
+      './src/**/*.js',
+      // etc.
+    ],
+  },
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+}
