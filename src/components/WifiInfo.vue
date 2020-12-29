7dfa71f82021e265979cef16a2b00171f9fdd94b
Vinicius Reis <luiz.vinicius73@gmail.com>
2020-12-29 17:49:16 -0300

    ✨ share page

--- src/components/WifiInfo.vue
+++ src/components/WifiInfo.vue
@@ -1,19 +1,65 @@
 <script>
 import { useWifi } from '../state/wifi'
+import { useQRCode } from '../state/qr-code'
+import { buildQRCodeData } from '../lib/qr-code'
 import IconWifi from './icons/Wifi.vue'
 import IconPrinter from './icons/Printer.vue'
+import IconShare from './icons/Share.vue'
+import { ref } from 'vue'
+
+const getFile = async (src) => {
+  const blob = await fetch(src).then((r) => r.blob())
+
+  return new File([blob], 'wifi.png', {
+    type: 'image/png'
+  })
+}
+
+const sharePage = async (src) => {
+  const file = await getFile(src)
+
+  const description = document
+    .querySelector('meta[name="description"]').content
+
+  return navigator.share({
+    title: document.title,
+    text: description,
+    url: window.location.href,
+    files: [file]
+  })
+}
 
 export default {
   name: 'wifi-info',
-  components: { IconWifi, IconPrinter },
+  components: { IconWifi, IconPrinter, IconShare },
   setup () {
     const { state } = useWifi()
+
+    const shareError = ref(null)
+
+    const { src: imageSrc } = useQRCode(() => buildQRCodeData(state))
+
     const print = () => {
       window.print()
     }
 
+    const share = () => {
+      shareError.value = null
+
+      sharePage(imageSrc.value)
+        .then(() => {
+          console.log('Shared')
+        })
+        .catch(err => {
+          shareError.value = err.message
+          console.warn(err)
+        })
+    }
+
     return {
       print,
+      shareError,
+      share: navigator.share ? share : false,
       state
     }
   }
@@ -37,11 +83,19 @@ export default {
       <dt>Password</dt>
       <dd>{{ state.password }}</dd>
     </dl>
-    <div class="hide-on-print text-center">
+    <div class="hide-on-print action-buttons text-center">
       <hr class="border-vue-green">
       <button @click="print">
         <IconPrinter />
         Print your QR Code
+      </button>
+
+      <button
+        :class="{ error: !!shareError }"
+        v-if="share"
+        @click="share">
+        <IconShare />
+        {{ shareError ? shareError : 'Share' }}
       </button>
     </div>
   </section>
@@ -58,5 +112,11 @@ dl dd {
   font-family: monospace;
   margin-bottom: 0.8em;
   padding-left: 0.9em;
+}
+.action-buttons button {
+  margin-right: 0.5em;
+}
+.action-buttons button:last-child {
+  margin-right: 0;
 }
 </style>
---
+++ src/components/icons/Share.vue
@@ -0,0 +1,29 @@
+<script>
+export default {
+  name: 'IconShare',
+  props: {
+    color: {
+      type: String,
+      default: '#fff'
+    }
+  }
+}
+</script>
+
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg"
+    width="44"
+    height="44"
+    viewBox="0 0 24 24"
+    stroke-width="1.5"
+    :stroke="color"
+    fill="none"
+    stroke-linecap="round"
+    stroke-linejoin="round">
+    <path d="M0 0h24v24H0z" stroke="none"/>
+    <circle cx="6" cy="12" r="3"/>
+    <circle cx="18" cy="6" r="3"/>
+    <circle cx="18" cy="18" r="3"/>
+    <path d="M8.7 10.7l6.6-3.4M8.7 13.3l6.6 3.4"/>
+  </svg>
+</template>
